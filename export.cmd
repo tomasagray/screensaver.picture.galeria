60bfb6c0819b9296b5797728de0eadc94398f04f
Tomás Gray <tomas.a.gray@gmail.com>
2024-06-14 16:38:29 -0700

    Add scripts, gitignore

---
+++ export.cmd
@@ -0,0 +1,43 @@
+:: Exports plugin as a .zip file for installation in Kodi
+@echo off
+
+:: Zip path
+Set "zip-path=C:\Program Files\7-Zip"
+Set "zip=%zip-path%\7z.exe"
+
+:: Kodi addon directory
+Set "export-dir=%1"
+Set "work-dir=%~dp0"
+:: Top-level files & dirs
+Set "main=%work-dir%\default.py"
+Set "fanart=%work-dir%\fanart.jpg"
+Set "icon=%work-dir%\icon.png"
+Set "addon=%work-dir%\addon.xml"
+Set "lic=%work-dir%\LICENSE"
+Set "lib=%work-dir%\lib"
+Set "resources=%work-dir%\resources"
+
+:: Zip plugin
+Set "archive=%work-dir%\screensaver.galeria"
+:: Create tmp dir
+echo Creating temporary directory...
+IF EXIST %archive% (rmdir /S /Q %archive%)
+mkdir %archive%
+:: Copy files to tmp dir
+echo Copying files to tmp dir: %archive%...
+COPY %main% "%archive%\"
+COPY %addon% "%archive%\"
+COPY %fanart% "%archive%\"
+COPY %icon% "%archive%\"
+COPY %lic% "%archive%\"
+XCOPY /e /y /q %lib% "%archive%\lib\"
+XCOPY /e /y /q %resources% "%archive%\resources\"
+echo Zipping plugin...
+"%zip%" a -tzip "%archive%.zip" "%archive%"
+:: Copy archive to export dir
+echo Exporting plugin to: %export-dir%...
+MOVE "%archive%.zip" %export-dir%
+:: Remove temp files
+echo Removing temporary files...
+rmdir /s/q %archive%
+echo Done.
---
+++ install.cmd
@@ -0,0 +1,25 @@
+:: Installs (updates) plugin files in Kodi installation directory.
+@echo off
+
+echo Installing Galeria for Kodi...
+:: Kodi addon directory
+Set "kodi-dir=%APPDATA%\Kodi\addons\screensaver.galeria\"
+Set "work-dir=%~dp0"
+:: Top-level files & dirs
+Set "main=%work-dir%\default.py"
+Set "fanart=%work-dir%\fanart.jpg"
+Set "icon=%work-dir%\icon.png"
+Set "addon=%work-dir%\addon.xml"
+Set "lib=%work-dir%\lib"
+Set "resources=%work-dir%\resources"
+
+echo Copying data from %main% to %kodi-dir%...
+echo Installing addon...
+COPY %main% %kodi-dir%
+COPY %fanart% %kodi-dir%
+COPY %icon% %kodi-dir%
+COPY %addon% %kodi-dir%
+echo Installing resources...
+XCOPY /y /s /q %lib% "%kodi-dir%lib\"
+XCOPY /y /s /q %resources% "%kodi-dir%resources\"
+echo Done.
